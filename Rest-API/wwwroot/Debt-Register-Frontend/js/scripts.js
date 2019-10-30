4c1aa289fef63cc86732812ead4d45d92b0a5590
Woolfer <arek.pazola@op.pl>
2019-10-30 21:10:37 +0100

    Initial commitos

--- Rest-API/wwwroot/Debt-Register-Frontend/js/scripts.js
+++ Rest-API/wwwroot/Debt-Register-Frontend/js/scripts.js
@@ -1,21 +1,29 @@
+var isLoggedIn = false;
+Console.log("dsadsa");
 $('#inp').click(function(e) {
-        var myData = {
-            Username: $('#username').val(),
-            Email: $('#email').val(),
-            Password: $('#password').val(),
-            FullName: $('#fullname').val()
-        };
-        console.log(myData);
-        $.ajax({
-            type: 'POST',
-            url: 'https://localhost:44379/api/User/Register',
-            dataType: 'json',
-            data: JSON.stringify(myData),
-            contentType: 'application/json',
-            success: function(data) {
-                alert(data)
-            }
-        }); 
+    var myData = {
+        Username: $('#username').val(),
+        Email: $('#email').val(),
+        Password: $('#password').val(),
+        FullName: $('#fullname').val()
+    };
+    console.log(myData);
+    $.ajax({
+        type: 'POST',
+        url: 'https://localhost:44379/api/User/Register',
+        dataType: 'json',
+        data: JSON.stringify(myData),
+        contentType: 'application/json',
+        success: function(data) {
+            alert(data);
+        }
+    });
 
-        return false;
-    })
+    return false;
+});
+var modal = document.querySelector(".login-modal");
+var button = document.querySelector("#account1");
+button.addEventListener("click", function (e) {
+    Console.log("hello");
+    modal.classList.toggle("show-modal");
+});
