f2fc3de6acf9a9072957e03c56e921c40539b05b
Woolfer <arek.pazola1998@gmail.com>
2019-11-09 17:33:04 +0100

    minor changes

---
+++ Rest-API/wwwroot/Debt-Register-Frontend/js/Borrowed.js
@@ -0,0 +1,34 @@
+const apiURL = "https://localhost:44379/api"
+
+$(document).ready(function(){
+    $.ajax({
+        type: 'GET',
+        url: `${apiURL}/Debt/1/Borrowed`,
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function(data) {
+            populateLastDebtsTable( document.getElementById("borrowedDebtsTableBody"), data)
+        },
+        error:  function(){
+            alert("Failed to load resources to last debts tables, check your internet connection!");
+        }
+    });
+});
+
+function populateLastDebtsTable(tableBody, data){
+    let myData = data;
+    for(let row in data)
+    {
+        let tableRow = tableBody.insertRow();
+        startDateCell = tableRow.insertCell();
+        startDateCell.innerHTML = new Date(data[row].debtStartDate).toLocaleDateString();
+        valueCell = tableRow.insertCell();
+        valueCell.innerHTML = data[row].value;
+        descriptionCell = tableRow.insertCell();
+        descriptionCell.innerHTML = data[row].description;
+        payedCell = tableRow.insertCell();
+        payedCell.innerHTML = data[row].payed == 'true';
+        borrowerCell = tableRow.insertCell();
+        borrowerCell.innerHTML = data[row].borrowerId;
+    }
+}
---
+++ Rest-API/wwwroot/Debt-Register-Frontend/js/Shared.js
@@ -0,0 +1,10 @@
+function showLastDebts(element)
+{
+    $(".last-and-to-person .highlighted").removeClass("highlighted");
+    $(element).addClass("highlighted");
+}
+function showDebtsToPerson(element)
+{
+    $(".last-and-to-person .highlighted").removeClass("highlighted");
+    $(element).addClass("highlighted");
+}
