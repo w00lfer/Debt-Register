71ee6017711918cc8b1f00eaa4dda8fa24d9768a
Woolfer <arek.pazola1998@gmail.com>
2020-04-14 12:47:30 +0200

    Add Angular to project

--- DebtRegister-SPA/src/app/app.module.ts
+++ DebtRegister-SPA/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {MatCheckboxModule} from '@angular/material/checkbox';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from '@angular/material/tabs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +14,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatCheckboxModule
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
